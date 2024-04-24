4948b16821ca40626ce6dcbc9dc872fb0e6825bc
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-24 18:23:08 +0000

    Done with work!

--- js/script.js
+++ js/script.js
@@ -4,26 +4,25 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
+let randomNumber;
+
+function initialize() {
+  randomNumber = Math.floor(Math.random() * 6) + 1;
+}
 
 /** 
  * This function checks if the user has selected the random number from above
  */
-
 function checkNumber() {
   //input
-  const userNumber = parseInt(document.getElementById("user-number").value)
+  const userNumber = parseInt(document.getElementById("user-number").value);
 
   //process
-  if (userNumber == randomNumber) {
+  if (userNumber === randomNumber) {
     //output
-    document.getElementById("result").innerHTML = 
-      "You have guessed the correct number!"
-  }
-
-  if (userNumber != randomNumber) {
+    document.getElementById("result").innerHTML = "You have guessed the correct number!";
+  } else {
     //output
-    document.getElementById("result").innerHTML =
-      "You have guessed the wrong number!"
+    document.getElementById("result").innerHTML = "You have guessed the wrong number!";
   }
 }
